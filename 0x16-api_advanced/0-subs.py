cf1335323b9dd182d309ed612eaa3642ca38c843
tarkavs <tarkavsunday@outlook.com>
2023-08-08 10:31:03 -0700

    advance api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and
-returns the number of subscribers (not active users, total subscribers)
-for a given subreddit.
-If an invalid subreddit is given, the function should return 0.
+Contains the number_of_subscribers function
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function to check number of subscribers """
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'chrome:0x16-api_advanced:v1'}
-
-    response = requests.get(url, headers=headers).json()
-    subs = response.get('data', {}).get('subscribers', 0)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
     return subs
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python3
-"""
-Write a recursive function that queries the Reddit API and
-returns a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None.
-"""
+"""Contains top_ten function"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after='', count=0):
-    """ Print first 10 titles """
-    if subreddit is None or type(subreddit) is not str:
-        return None
-
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'chrome:0x16-api_advanced:v1'}
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
     params = {
-        'after': after,
-        'count': count,
-        'limit': 100
-        }
-
+        "limit": 10
+    }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    if response.status_code != 200:
-        return None
-
-    response = response.json().get('data')
-    after = response.get('after')
-    count = response.get('dist')
-    [hot_list.append(c.get('data').get('title'))
-     for c in response.get('children')]
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-
-    return hot_list
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
